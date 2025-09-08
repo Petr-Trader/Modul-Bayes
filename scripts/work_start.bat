a45c9e0b6dacf6b15d4e2a6b5ec441eea3c9e3da
Petr-Trader <schramlp@gmail.com>
2025-09-08 20:23:48 +0200

    feat: přidány start/end skripty pro home/work prostředí

---
+++ scripts/work_start.bat
@@ -0,0 +1,12 @@
+@echo off
+cd /d C:\CLON_Git\Modul-Bayes
+git switch dev
+git pull origin dev
+if not exist .venv (
+  python -m venv .venv
+)
+call .venv\Scripts\activate
+if exist scripts\make_briefing.bat call scripts\make_briefing.bat
+start "" "https://github.com/Petr-Trader/Modul-Bayes/pulls"
+start "" "https://github.com/Petr-Trader/Modul-Bayes/actions"
+start "" code .
