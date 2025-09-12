d940eceba38709dffc73a485941b834fdce5c600
Petr-Trader <schramlp@gmail.com>
2025-09-12 09:40:54 +0200

    chore(scripts): auto-generate vypis.txt on start + ignore it

--- scripts/work_start.bat
+++ scripts/work_start.bat
@@ -10,3 +10,9 @@ if exist scripts\make_briefing.bat call scripts\make_briefing.bat
 start "" "https://github.com/Petr-Trader/Modul-Bayes/pulls"
 start "" "https://github.com/Petr-Trader/Modul-Bayes/actions"
 start "" code .
+REM === Auto: plný výpis repa do vypis.txt (v1.00) ===
+REM %~dp0 = cesta k tomuto .bat; .. = kořen repa (protože .bat je ve složce scripts)
+set "_REPO_ROOT=%~dp0.."
+pushd "%_REPO_ROOT%" >nul
+dir /a /s /b > "vypis.txt"
+popd >nul
